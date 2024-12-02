3b55ef10941c1f10caf64d4d0325ccaa4b00673e
lucad-28 <lucadev.rll28@gmail.com>
2024-12-02 02:05:09 -0500

    fix warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ViewerView } from "./components/ViewerView";
 import { EditorView } from "./components/EditorView";
 
 const App: React.FC = () => {
   const [mode, setMode] = useState<"view" | "edit">("edit");
+
+  useEffect(() => {
+    setMode("view");
+  }, []);
+
   return (
     <div className="relative w-lvw h-lvh">
       {mode === "view" ? <ViewerView /> : <EditorView />}
